e38224d00a591b84f738ebd7a8e961f016ae7d5e
tomasfv <tomasfernandezvaldes@gmail.com>
2026-09-11 14:17:58 -0300

    update frontend test

--- frontend/src/__tests__/setup.ts
+++ frontend/src/__tests__/setup.ts
@@ -3,3 +3,15 @@ import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder as any;
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args: any[]) => {
+    if (typeof args[0] === 'string' && args[0].includes('not wrapped in act')) return;
+    originalError(...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
